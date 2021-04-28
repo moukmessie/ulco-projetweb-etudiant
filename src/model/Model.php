61f45bb188f704d1f67ded869dbc3ec01f4c8815
moukmessie <moukmessie@gmail.com>
2021-04-28 21:06:33 +0200

    last commit projet

--- src/model/Model.php
+++ src/model/Model.php
@@ -6,10 +6,17 @@ class Model {
 
   static function connect()
   {
-    $dsn = "mysql:host=sql4.freesqldatabase.com;dbname=sql4408981;charset=UTF8";
-    $user = "sql4408981";
-    $pass = "wUy6ckkQ1n";
+    $dsn = "mysql: localhost:3306;dbname=webstore;charset=UTF8";
+    $user = "root";
+    $pass = "";
     return new \PDO($dsn, $user, $pass);
+
+      /*$dsn = "mysql:host=sql4.freesqldatabase.com;dbname=sql4408981;charset=UTF8";
+      $user = "sql4408981";
+      $pass = "wUy6ckkQ1n";
+      return new \PDO($dsn, $user, $pass);*/
   }
+
+
 
 }
