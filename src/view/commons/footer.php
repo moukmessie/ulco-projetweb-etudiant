be5b2b2f09e65d65eb5c906032c8d820f433297d
moukmessie <moukmessie@gmail.com>
2022-02-08 01:57:42 +0100

    change mysql database to sqlite

--- src/view/commons/footer.php
+++ src/view/commons/footer.php
@@ -3,5 +3,5 @@
     <img style="width: 32px" src="/public/images/avatar.png" alt="3">
     <p>Projet</p>
     <p>Messie MOUKIMOU</p>
-    <p>L2 Informatique - 2021</p>
+    <p> copyright © <?= date('Y')?></p>
 </footer>
